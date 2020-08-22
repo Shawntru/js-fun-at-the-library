7518b59c48f92004712348ba6e12b658705bb628
Shawn Truesdale <shawnctruesdale@gmail.com>
2020-08-22 15:50:32 -0600

    Finish shelf.js and shelf-test.js

--- src/shelf.js
+++ src/shelf.js
@@ -1,8 +1,42 @@
+function shelfBook(name, shelf) {
+  if (shelf.length >= 3) {
+    return;
+  }
+  shelf.unshift(name);
+}
 
+function unshelfBook(name, shelf) {
+  for (i = 0; i < shelf.length; i ++) {
+    if (shelf[i].title === name) {
+      shelf.splice(i, 1);
+      return;
+    }
+  }
+}
+
+function listTitles(shelf) {
+  var titles = ""
+  for (i = 0; i < shelf.length; i ++) {
+    titles += `${shelf[i].title}`;
+    if ((i + 1) < shelf.length) {
+      titles += ", ";
+    }
+  }
+  return titles;
+}
+
+function searchShelf(shelf, title) {
+  for (i = 0; i < shelf.length; i ++) {
+    if (shelf[i].title === title) {
+      return true;
+    }
+  }
+  return false;
+}
 
 module.exports = {
-  // shelfBook: shelfBook,
-  // unshelfBook: unshelfBook,
-  // listTitles: listTitles,
-  // searchShelf: searchShelf
+  shelfBook: shelfBook,
+  unshelfBook: unshelfBook,
+  listTitles: listTitles,
+  searchShelf: searchShelf
 };
